e7fdf587d9996f754c3965efb44e8bdf4270a2f0
Ivanildo Cândido Bezerra <systechtecnologia@gmail.com>
2025-04-30 14:12:01 -0300

    adiciona variáveis de ambientes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-const status = (req, res) => {
+const  database =  require("../../../../infra/database");
+const status = async (req, res) => {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   res.status(200).json({
     status: "ok",
     message: "API is running",
